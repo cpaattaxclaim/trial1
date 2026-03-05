94f049d3e955ff81020a524b789d24602c0189ea
cpaattaxclaim <cpa@taxclaim.co>
2026-03-05 20:31:36 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async"; // 1. Added import
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/* 2. Wrap App in HelmetProvider so SEO tags work */}
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
