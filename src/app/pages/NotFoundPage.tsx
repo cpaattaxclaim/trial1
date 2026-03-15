ab9b5ac0c765a392c36b6f244320a0d9536412e5
cpaattaxclaim <cpa@taxclaim.co>
2026-03-16 01:36:47 +0530

    Update NotFoundPage.tsx

--- src/app/pages/NotFoundPage.tsx
+++ src/app/pages/NotFoundPage.tsx
@@ -8,6 +8,7 @@ export function NotFoundPage() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
+      <main>
 
       <section className="py-32">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
@@ -84,6 +85,7 @@ export function NotFoundPage() {
         </div>
       </section>
 
+      </main>
       <Footer />
     </div>
   );
