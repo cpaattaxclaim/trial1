bec6a0c54b82c2dc7a79f10083dfb141ba5b5ee8
cpaattaxclaim <cpa@taxclaim.co>
2026-03-25 11:19:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePrerender } from 'vite-plugin-prerender'
+import VitePrerender from 'vite-plugin-prerender'
 
 export default defineConfig({
   plugins: [
