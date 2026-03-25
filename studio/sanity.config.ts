a91d1fc2d06d3b91b85edebf68c8078086c7c6e6
cpaattaxclaim <cpa@taxclaim.co>
2026-03-26 00:51:37 +0530

    Update sanity.config.ts

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -3,6 +3,10 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
 
+console.log('POST:', post)
+console.log('AUTHOR:', author)
+console.log('CATEGORY:', category)
+
 export default defineConfig({
   name: 'default',
   title: 'TaxClaim Blog',
